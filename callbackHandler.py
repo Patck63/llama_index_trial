1db9859e1e1fa7f71a26c0d4adb85eac2be1d61d
Patck63 <85246779+Patck63@users.noreply.github.com>
2023-07-31 15:36:38 +0800

    feat: handles Chinese documents

---
+++ callbackHandler.py
@@ -0,0 +1,39 @@
+from typing import Any, Dict, List, Optional
+from llama_index.callbacks.schema import CBEventType
+from llama_index.callbacks.base import CallbackManager, BaseCallbackHandler
+
+
+class CallbackHandler(BaseCallbackHandler):
+    """Base callback handler that can be used to track event starts and ends."""
+
+    def on_event_start(
+        self,
+        event_type: CBEventType,
+        payload: Optional[Dict[str, Any]] = None,
+        event_id: str = "",
+        **kwargs: Any,
+    ) -> str:
+        """Run when an event starts and return id of event."""
+        print("start", event_type, payload)
+
+    def on_event_end(
+        self,
+        event_type: CBEventType,
+        payload: Optional[Dict[str, Any]] = None,
+        event_id: str = "",
+        **kwargs: Any,
+    ) -> None:
+        """Run when an event ends."""
+        print("end", event_type, payload)
+
+    def start_trace(self, trace_id: Optional[str] = None) -> None:
+        """Run when an overall trace is launched."""
+        pass
+
+    def end_trace(
+        self,
+        trace_id: Optional[str] = None,
+        trace_map: Optional[Dict[str, List[str]]] = None,
+    ) -> None:
+        """Run when an overall trace is exited."""
+        pass
--- test.py
+++ test.py
@@ -2,10 +2,20 @@ from llama_index import StorageContext, load_index_from_storage
 import os
 import openai
 from dotenv import load_dotenv
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, LangchainEmbedding, ServiceContext, load_index_from_storage
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, LangchainEmbedding, ServiceContext, load_index_from_storage
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
+from llama_index.callbacks import TokenCountingHandler, CallbackManager
 
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.langchain_helpers.text_splitter import SentenceSplitter
+
+from llama_hub.file.cjk_pdf.base import CJKPDFReader
+from pathlib import Path
+from llama_index import download_loader
+
+
+from callbackHandler import CallbackHandler
 
 # Load environment variables (set OPENAI_API_KEY and OPENAI_API_BASE in .env)
 load_dotenv()
@@ -40,26 +50,59 @@ max_chunk_overlap = 0.1
 prompt_helper = PromptHelper(
     max_input_size, num_output, max_chunk_overlap, chunk_size_limit)
 
+
+# Create an instance of the TokenCountingHandler
+token_counter = TokenCountingHandler()
+
+# Add the TokenCountingHandler to the CallbackManager
+callback_manager = CallbackManager([token_counter, CallbackHandler([], [])])
+
+text_splitter = SentenceSplitter(
+    separator="。",
+    chunk_size=1024,
+    chunk_overlap=20,
+    backup_separators=["\n"],
+    paragraph_separator="\n\n\n"
+)
+node_parser = SimpleNodeParser(text_splitter=text_splitter)
+
 service_context = ServiceContext.from_defaults(
-    llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
+    llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm, callback_manager=callback_manager, node_parser=node_parser)
 
-# rebuild storage context
-storage_context = StorageContext.from_defaults(persist_dir='./storage')
-# load index
-index = load_index_from_storage(
-    storage_context, service_context=service_context)
-
-# # check if index is loaded
-if index is None:
-    print("index is not loaded")
-    documents = SimpleDirectoryReader('data').load_data()
+# check if storage is present
+if not os.path.exists('./storage'):
+    print("index is not present or loaded")
+    # documents = SimpleDirectoryReader('data').load_data()
+    CJKPDFReader = download_loader("CJKPDFReader")
+    loader = CJKPDFReader()
+    documents = loader.load_data(
+        file=Path('./data/Colorectal-July22C-Revamp-Web.pdf'))
 
     # Create index
-    index = GPTVectorStoreIndex.from_documents(
-        documents, service_context=service_context)
+    index = VectorStoreIndex.from_documents(
+        documents, service_context=service_context, show_progress=True)
     # persist index
     index.storage_context.persist()
+else:
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(
+        persist_dir='./storage')
+    # load index
+    index = load_index_from_storage(
+        storage_context, service_context=service_context)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
+response = query_engine.query("用中文回答，大腸癌的成因")
+# response = query_engine.query("what is the cause of colorectal cancer?")
+# response = query_engine.query("what is colorectal cancer?")
 print("\nQuery response: ", response)
+
+# Get the token counts
+# embedding_tokens = token_counter.total_embedding_token_count
+# llm_prompt_tokens = token_counter.prompt_llm_token_count
+# llm_completion_tokens = token_counter.completion_llm_token_count
+# total_llm_tokens = token_counter.total_llm_token_count
+# print("embedding_tokens: ", embedding_tokens)
+# print("llm_prompt_tokens: ", llm_prompt_tokens)
+# print("llm_completion_tokens: ", llm_completion_tokens)
+# print("total_llm_tokens: ", total_llm_tokens)
